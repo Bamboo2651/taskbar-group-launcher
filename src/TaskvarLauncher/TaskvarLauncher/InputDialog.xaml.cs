69c092e9826afffa6dc09f636012cc99c2175745
tkhy2651 <tah331925@gmail.com>
2026-04-05 22:13:57 +0900

    グループ追加機能と、バリデーション実装した

---
+++ src/TaskvarLauncher/TaskvarLauncher/InputDialog.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Printing;
+using System.Windows;
+
+namespace TaskbarLauncher
+{
+    public partial class InputDialog : Window
+    {
+        public string Answer { get; private set; } = "";
+
+        public InputDialog(string title)
+        {
+            InitializeComponent();
+            TitleText.Text = title;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(InputBox.Text))
+            {
+                InputBox.BorderBrush = System.Windows.Media.Brushes.Red;
+                return;
+            }
+            Answer = InputBox.Text.Trim();
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
--- src/TaskvarLauncher/TaskvarLauncher/MainWindow.xaml.cs
+++ src/TaskvarLauncher/TaskvarLauncher/MainWindow.xaml.cs
@@ -1,12 +1,39 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using TaskbarLauncher.Models;
 
 namespace TaskbarLauncher
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<GroupConfig> Groups { get; set; } = new();
+        private ConfigManager _configManager = new();
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            LoadGroups();
+        }
+
+        private void LoadGroups()
+        {
+            var groups = _configManager.LoadGroups();
+            Groups.Clear();
+            foreach (var group in groups)
+                Groups.Add(group);
+        }
+
+        private void AddGroup_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new InputDialog("グループ名を入力してください");
+            dialog.Owner = this;
+            if (dialog.ShowDialog() == true)
+            {
+                var newGroup = new GroupConfig { Name = dialog.Answer };
+                Groups.Add(newGroup);
+                _configManager.SaveGroups(new List<GroupConfig>(Groups));
+            }
         }
     }
 }
