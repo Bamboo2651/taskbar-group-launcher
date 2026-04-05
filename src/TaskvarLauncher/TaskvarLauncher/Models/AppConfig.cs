138ef028b29e9f228b90775714551e0a00aecda6
tkhy2651 <tah331925@gmail.com>
2026-04-05 21:54:10 +0900

    GroupConfig・AppConfigモデルを作成

---
+++ src/TaskvarLauncher/TaskvarLauncher/Models/AppConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskbarLauncher.Models
+{
+    public class AppConfig
+    {
+        public string Name { get; set; } = "";
+        public string Path { get; set; } = "";
+    }
+}
---
+++ src/TaskvarLauncher/TaskvarLauncher/Models/GroupConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TaskbarLauncher.Models
+{
+    public class GroupConfig
+    {
+        public string Name { get; set; } = "";
+        public List<AppConfig> Apps { get; set; } = new List<AppConfig>();
+    }
+}
