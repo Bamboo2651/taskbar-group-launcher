8d607c9ae25abc7f595da7937dc38a87b6777e99
tkhy2651 <tah331925@gmail.com>
2026-04-07 12:38:19 +0900

    画像表示

--- src/TaskvarLauncher/TaskvarLauncher/Models/AppConfig.cs
+++ src/TaskvarLauncher/TaskvarLauncher/Models/AppConfig.cs
@@ -1,8 +1,38 @@
-﻿namespace TaskbarLauncher.Models
+﻿using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace TaskbarLauncher.Models
 {
     public class AppConfig
     {
         public string Name { get; set; } = "";
         public string Path { get; set; } = "";
+
+        public BitmapSource? Icon
+        {
+            get
+            {
+                if (!File.Exists(Path))
+                    return null;
+
+                try
+                {
+                    var icon = System.Drawing.Icon.ExtractAssociatedIcon(Path);
+                    if (icon == null) return null;
+
+                    return Imaging.CreateBitmapSourceFromHIcon(
+                        icon.Handle,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+                }
+                catch
+                {
+                    return null;
+                }
+            }
+        }
     }
 }
